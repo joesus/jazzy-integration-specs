dbb64ba768904add644fbcf87473f9394ca752ce
joesusnick <joesusnick@gmail.com>
2019-10-08 21:27:42 -0700

    Adding top-level Swift class to MiscJazzyObjCFeatures

---
+++ misc_jazzy_objc_features/before/MiscJazzyObjCFeatures/SwiftTopLevelClass.swift
@@ -0,0 +1,12 @@
+//
+//  TopLevelSwiftClass.swift
+//  MiscJazzyObjCFeatures
+//
+//  Created by Joe Susnick on 10/8/19.
+//  Copyright © 2019 PSPDFKit GmbH. All rights reserved.
+//
+
+import Foundation
+
+public
+class TopLevelSwiftClass {}
